f442dbb8141eb680986cbe5f41be97a9e4d07699
BRULE Herman <alpha_one_x86@first-world.info>
2012-05-05 03:27:42 +0200

    Fix previous code

--- server/base/ClientMapManagement/ClientMapManagement.h
+++ server/base/ClientMapManagement/ClientMapManagement.h
@@ -54,10 +54,9 @@ public slots:
 	//map slots, transmited by the current ClientNetworkRead
 	void put_on_the_map(const quint32 &player_id,Map_final *map,const quint16 &x,const quint16 &y,const Orientation &orientation,const quint16 &speed);
 	virtual void moveThePlayer(const quint8 &previousMovedUnit,const Direction &direction);
-	//normal slots
-	void askIfIsReadyToStop();
 private slots:
 	void purgeBuffer();
+	void extraStop();
 private:
 	//info linked
 	Orientation			at_start_orientation;
