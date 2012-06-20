77df11c9ab2a6531333d937961c88a4be8280793
BRULE Herman <alpha_one_x86@first-world.info>
2012-06-20 12:13:48 +0200

    Minor update

--- general/base/MoveOnTheMap.h
+++ general/base/MoveOnTheMap.h
@@ -1,6 +1,7 @@
 #include <QObject>
 
 #include "GeneralStructures.h"
+#include "Map.h"
 
 #ifndef MOVEONTHEMAP_H
 #define MOVEONTHEMAP_H
@@ -15,6 +16,11 @@ protected:
 	virtual void send_player_move(const quint8 &moved_unit,const Direction &the_new_direction) = 0;
 	Direction last_direction;
 	quint8 last_step;
+	Map *map;
+	quint16 x,y;
+protected:
+	bool canGoTo(Direction direction);
+	void move(Direction direction);
 };
 
 #endif // MOVEONTHEMAP_H
