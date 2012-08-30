b4f11d1f287ae1c586f68912b76f5ff10b875bfa
BRULE Herman <alpha_one_x86@first-world.info>
2012-08-30 03:00:15 +0200

    [Add] Update the map visualiser (tools)

---
+++ tools/map-visualiser-qt/client/base/ClientStructures.h
@@ -0,0 +1,8 @@
+#ifndef POKECRAFT_STRUCTURES_CLIENT_H
+#define POKECRAFT_STRUCTURES_CLIENT_H
+
+#include <QObject>
+#include <QString>
+#include <QPixmap>
+
+#endif // STRUCTURES_CLIENT_H
---
+++ tools/map-visualiser-qt/general/base/Map_loader.h
@@ -0,0 +1,38 @@
+#ifndef POKECRAFT_MAP_LOADER_H
+#define POKECRAFT_MAP_LOADER_H
+
+#include <QObject>
+#include <QStringList>
+#include <QString>
+#include <QDomDocument>
+#include <QDomElement>
+#include <QFile>
+#include <QCoreApplication>
+#include <QVariant>
+#include <QMutex>
+#include <QTime>
+#include <QMutexLocker>
+
+#include "DebugClass.h"
+#include "GeneralStructures.h"
+#include "Map.h"
+
+namespace Pokecraft {
+class Map_loader : public QObject
+{
+	Q_OBJECT
+public:
+	explicit Map_loader();
+	~Map_loader();
+
+	Map_to_send map_to_send;
+	QString errorString();
+    bool tryLoadMap(const QString &fileName);
+	QString resolvRelativeMap(const QString &fileName,const QString &link,const QString &datapackPath);
+private:
+	QByteArray decompress(const QByteArray &data, int expectedSize);
+	QString error;
+};
+}
+
+#endif // MAP_LOADER_H
