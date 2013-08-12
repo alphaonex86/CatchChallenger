209ae04fad88a6765dad7c99f85be653661ca269
BRULE Herman <alpha_one_x86@first-world.info>
2013-08-12 12:55:44 +0200

    Delete the old datapack, is external now

--- datapack/map/tileset/animation.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="animation" tilewidth="16" tileheight="16">
- <image source="animation.png" trans="000000" width="512" height="512"/>
- <tile id="0">
-  <properties>
-   <property name="animation" value="500ms;4frames"/>
-  </properties>
- </tile>
-</tileset>
