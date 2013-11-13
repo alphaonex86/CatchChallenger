c42879474fb63eb9f5bde50f676f91adcf369c39
BRULE Herman <alpha_one_x86@first-world.info>
2013-11-13 23:55:30 +0100

    [Clean] Update the datapack cleaner (tools)

--- tools/datapack-compressor/png-compress.sh
+++ tools/datapack-compressor/png-compress.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if [ -x /usr/bin/pngquant ]
 then
+	TEMPRANDOM=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
 	for VARIABLE in `find ./ -name '*.png' -type f`
 	do
-		cat "${VARIABLE}" | /usr/bin/pngquant - --speed 1 > /tmp/tmp.png
-		mv /tmp/tmp.png "${VARIABLE}"
+		cat "${VARIABLE}" | /usr/bin/pngquant - --speed 1 > /tmp/tmp${TEMPRANDOM}.png
+		mv /tmp/tmp${TEMPRANDOM}.png "${VARIABLE}"
 	done
 fi
 
--- tools/datapack-compressor/xml-compress.sh
+++ tools/datapack-compressor/xml-compress.sh
@@ -6,5 +6,15 @@ then
 		/usr/bin/php "${BASH_SOURCE[0]}.php" "${VARIABLE}" > /tmp/file
 		mv /tmp/file "${VARIABLE}"
 	done
+	for VARIABLE in `find ./ -name '*.tmx' -type f`
+	do
+		/usr/bin/php "${BASH_SOURCE[0]}.php" "${VARIABLE}" > /tmp/file
+		mv /tmp/file "${VARIABLE}"
+	done
+	for VARIABLE in `find ./ -name '*.tsx' -type f`
+	do
+		/usr/bin/php "${BASH_SOURCE[0]}.php" "${VARIABLE}" > /tmp/file
+		mv /tmp/file "${VARIABLE}"
+	done
 fi
 
