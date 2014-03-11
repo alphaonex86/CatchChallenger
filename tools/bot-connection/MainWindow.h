a497607719e03b0d285111cefb46c37b4a406c7c
BRULE Herman <alpha_one_x86@first-world.info>
2014-03-11 18:48:22 +0100

    [Add] Bot connection tool

---
+++ tools/bot-connection/MainWindow.h
@@ -0,0 +1,69 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QTimer>
+#include <QSemaphore>
+#include <QByteArray>
+#include <QSettings>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../../general/base/MoveOnTheMap.h"
+#include "../../general/base/QFakeSocket.h"
+#include "../../general/base/ConnectedSocket.h"
+#include "../../server/base/Api_client_virtual.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow, public CatchChallenger::MoveOnTheMap
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void start_step();
+    void show_details();
+    QSslSocket *sslSocket;
+    CatchChallenger::ConnectedSocket socket;
+private:
+    CatchChallenger::Api_client_virtual api;
+    //debug
+    bool details;
+    //virtual action
+    void send_player_move(const quint8 &moved_unit,const CatchChallenger::Direction &the_direction);
+
+    static int index_loop,loop_size;
+    static QSemaphore wait_to_stop;
+    bool do_move;
+    QSettings settings;
+    bool haveShowDisconnectionReason;
+public slots:
+    void stop_move();
+    void doMove();
+private slots:
+    void random_new_step();
+    void insert_player(const CatchChallenger::Player_public_informations &player,const quint32 &mapId,const quint16 &x,const quint16 &y,const CatchChallenger::Direction &direction);
+    void haveCharacter();
+    void logged(const QList<CatchChallenger::CharacterEntry> &characterEntryList);
+    void have_current_player_info(const CatchChallenger::Player_private_and_public_informations &informations);
+    void newError(QString error,QString detailedError);
+    void newSocketError(QAbstractSocket::SocketError error);
+    void disconnected();
+    void tryLink();
+    void on_connect_clicked();
+    void sslErrors(const QList<QSslError> &errors);
+    void on_characterSelect_clicked();
+
+signals:
+    void isDisconnected();
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
---
+++ tools/bot-connection/bot-connection.pro
@@ -0,0 +1,18 @@
+include(../../general/general.pri)
+include(../../server/catchchallenger-server.pri)
+
+QT       += core gui xml network sql
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = bot-connection
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp
+HEADERS  += MainWindow.h
+
+FORMS    += MainWindow.ui
+
+win32:RESOURCES += $$PWD/../../general/base/resources/resources-windows-qt-plugin.qrc
