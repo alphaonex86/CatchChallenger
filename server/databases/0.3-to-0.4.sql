bf4479c5e01383ea6cacae54068021a103a956b0
BRULE Herman <alpha_one_x86@first-world.info>
2014-05-06 20:32:04 +0200

    fix account creation

--- server/databases/0.3-to-0.4.sql
+++ server/databases/0.3-to-0.4.sql
@@ -1,2 +1,3 @@
-ALTER TABLE  `account` CHANGE  `login`  `login` VARBINARY(28) NOT NULL ,
-CHANGE  `password`  `password` VARBINARY(28) NOT NULL
+ALTER TABLE  `account` CHANGE  `login`  `login` VARBINARY(28) NOT NULL ,CHANGE  `password`  `password` VARBINARY(28) NOT NULL;
+
+ALTER TABLE  `plant` CHANGE  `map`  `map` VARCHAR( 64 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
