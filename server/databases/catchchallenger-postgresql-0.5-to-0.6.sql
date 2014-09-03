fab5face7977e20334b4af238f44e1fb06358993
BRULE Herman <alpha_one_x86@first-world.info>
2014-09-03 17:36:05 +0200

    Fix db update

--- server/databases/catchchallenger-postgresql-0.5-to-0.6.sql
+++ server/databases/catchchallenger-postgresql-0.5-to-0.6.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "dictionary_itemonoap" (
+CREATE TABLE "dictionary_itemonmap" (
     id integer NOT NULL,
     map text,
     x smallint,
     y smallint
 );
 ALTER TABLE ONLY "dictionary_itemonmap" ADD CONSTRAINT "dictionary_itemOnMap_pkey" PRIMARY KEY (id);
-CREATE INDEX "character_itemOnMap_index" ON "character_itemonmap" USING btree ("character"); 
+CREATE INDEX "character_itemOnMap_index" ON "character_itemonmap" USING btree ("character");
 CREATE TABLE "character_itemonmap" (
     "character" integer,
     "itemOnMap" smallint
