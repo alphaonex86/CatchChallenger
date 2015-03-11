b8904aea93f7a37be8a8ece582ccaf83a7168571
BRULE Herman <alpha_one_x86@first-world.info>
2015-03-11 10:10:14 +0100

    [Add] Split the remote databases (server)

---
+++ server/databases/mysql/catchchallenger-mysql-login.sql
@@ -0,0 +1,39 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `catchchallenger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varbinary(28) NOT NULL,
+  `password` varbinary(28) NOT NULL,
+  `date` int(11) unsigned NOT NULL,
+  `email` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_register`
+--
+
+CREATE TABLE IF NOT EXISTS `account_register` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `key` text NOT NULL,
+  `date` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login_2` (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ server/databases/postgresql/catchchallenger-postgresql-login.sql
@@ -0,0 +1,72 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: account; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE account (
+    id integer NOT NULL,
+    login character varying(56),
+    password character varying(56),
+    date integer,
+    email text
+);
+
+
+--
+-- Name: account_register; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE account_register (
+    login character varying(128),
+    password character varying(128),
+    email character varying(64),
+    key text,
+    date integer
+);
+
+
+--
+-- Name: account_login_key; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY account
+    ADD CONSTRAINT account_login_key UNIQUE (login);
+
+
+--
+-- Name: account_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY account
+    ADD CONSTRAINT account_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: account_register_login_key; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY account_register
+    ADD CONSTRAINT account_register_login_key UNIQUE (login);
+
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
