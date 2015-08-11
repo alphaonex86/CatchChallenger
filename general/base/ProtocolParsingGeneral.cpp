32ada3d3c4194df4f9d3c970fca73b45f308c7d9
BRULE Herman <alpha_one_x86@first-world.info>
2015-08-11 11:07:12 +0200

    [Change] More stuff for the compression (shared lib)

--- general/base/ProtocolParsingGeneral.cpp
+++ general/base/ProtocolParsingGeneral.cpp
@@ -121,6 +121,83 @@ QByteArray ProtocolParsingBase::lzmaCompress(QByteArray data)
         strm.avail_out = OUT_BUF_MAX;
         ret_xz = lzma_code (&strm, LZMA_FINISH);
 
+        if (ret_xz != LZMA_OK) {
+            // Once everything has been decoded successfully, the
+            // return value of lzma_code() will be LZMA_STREAM_END.
+            //
+            // It is important to check for LZMA_STREAM_END. Do not
+            // assume that getting ret != LZMA_OK would mean that
+            // everything has gone well or that when you aren't
+            // getting more output it must have successfully
+            // decoded everything.
+            if (ret == LZMA_STREAM_END)
+                return arr;
+
+            // It's not LZMA_OK nor LZMA_STREAM_END,
+            // so it must be an error code. See lzma/base.h
+            // (src/liblzma/api/lzma/base.h in the source package
+            // or e.g. /usr/include/lzma/base.h depending on the
+            // install prefix) for the list and documentation of
+            // possible values. Many values listen in lzma_ret
+            // enumeration aren't possible in this example, but
+            // can be made possible by enabling memory usage limit
+            // or adding flags to the decoder initialization.
+            const char *msg;
+            switch (ret) {
+            case LZMA_MEM_ERROR:
+                msg = "Memory allocation failed";
+                break;
+
+            case LZMA_FORMAT_ERROR:
+                // .xz magic bytes weren't found.
+                msg = "The input is not in the .xz format";
+                break;
+
+            case LZMA_OPTIONS_ERROR:
+                // For example, the headers specify a filter
+                // that isn't supported by this liblzma
+                // version (or it hasn't been enabled when
+                // building liblzma, but no-one sane does
+                // that unless building liblzma for an
+                // embedded system). Upgrading to a newer
+                // liblzma might help.
+                //
+                // Note that it is unlikely that the file has
+                // accidentally became corrupt if you get this
+                // error. The integrity of the .xz headers is
+                // always verified with a CRC32, so
+                // unintentionally corrupt files can be
+                // distinguished from unsupported files.
+                msg = "Unsupported compression options";
+                break;
+
+            case LZMA_DATA_ERROR:
+                msg = "Compressed file is corrupt";
+                break;
+
+            case LZMA_BUF_ERROR:
+                // Typically this error means that a valid
+                // file has got truncated, but it might also
+                // be a damaged part in the file that makes
+                // the decoder think the file is truncated.
+                // If you prefer, you can use the same error
+                // message for this as for LZMA_DATA_ERROR.
+                msg = "Compressed file is truncated or "
+                        "otherwise corrupt";
+                break;
+
+            default:
+                // This is most likely LZMA_PROG_ERROR.
+                msg = "Unknown error, possibly a bug";
+                break;
+            }
+
+            fprintf(stderr, "%s: Decoder error: "
+                    "%s (error code %u)\n",
+                    inname, msg, ret);
+            return QByteArray();
+        }
+
         out_len = OUT_BUF_MAX - strm.avail_out;
         arr.append((char*)out_buf, out_len);
         out_buf[0] = 0;
