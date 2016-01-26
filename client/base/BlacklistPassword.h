dcc644246576bb27b73e687bc928ecaa462998db
BRULE Herman <alpha_one_x86@first-world.info>
2016-01-26 02:22:48 +0100

    Add client pass blacklist

---
+++ client/base/BlacklistPassword.h
@@ -0,0 +1,12 @@
+#ifndef BlacklistPassword_H
+#define BlacklistPassword_H
+
+#include <array>
+
+class BlacklistPassword
+{
+public:
+    static std::array<std::string, 500> list;
+};
+
+#endif // BlacklistPassword_H
