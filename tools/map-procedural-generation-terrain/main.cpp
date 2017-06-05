b0277d587d3b93515eeecd1ba8c89f3768cede9f
BRULE Herman <alpha_one_x86@first-world.info>
2017-06-05 20:41:20 +0200

    [Change] Terrain transition (tools)

--- tools/map-procedural-generation-terrain/main.cpp
+++ tools/map-procedural-generation-terrain/main.cpp
@@ -426,6 +426,48 @@ unsigned int stringToTerrainInt(const QString &string)
         return 2;
 }
 
+Tiled::Tile * intToTile(const Terrain &grass,const Terrain &water,const Terrain &montain,const unsigned int &terrainInt)
+{
+    switch(terrainInt)
+    {
+        case 0:
+            return grass.tileset->addTile(grass.tileId);
+        break;
+        case 1:
+            return water.tileset->addTile(water.tileId);
+        break;
+        case 2:
+            return montain.tileset->addTile(montain.tileId);
+        break;
+    }
+    abort();
+    return NULL;
+}
+
+void load_terrainTransitionList(const Terrain &grass,const Terrain &water,const Terrain &montain,std::vector<TerrainTransition> &terrainTransitionList)
+{
+    unsigned int terrainTransitionIndex=0;
+    while(terrainTransitionIndex<terrainTransitionList.size())
+    {
+        //Tiled::Tile * from_type;
+        TerrainTransition &terrainTransition=terrainTransitionList.at(terrainTransitionIndex);
+        terrainTransition.from_type=intToTile(grass,water,montain,terrainTransition.tmp_from_type);
+        unsigned int to_type_Index=0;
+        while(to_type_Index<terrainTransition.tmp_to_type.size())
+        {
+            terrainTransition.to_type.push_back(intToTile(grass,water,montain,terrainTransition.tmp_to_type.at(terrainTransition)));
+            to_type_Index++;
+        }
+        //tempory value
+        QString tmp_transition_tsx;
+        std::vector<unsigned int> tmp_transition_tile;
+        //after map creation
+        std::vector<Tiled::Tile *> transition_tile;
+
+        terrainTransitionIndex++;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     // Avoid performance issues with X11 engine when rendering objects
@@ -664,6 +706,7 @@ int main(int argc, char *argv[])
             Tiled::Map tiledMap(Tiled::Map::Orientation::Orthogonal,totalWidth,totalHeight,16,16);
             QHash<QString,Tiled::Tileset *> cachedTileset;
             loadTileset(water,cachedTileset,tiledMap);loadTileset(grass,cachedTileset,tiledMap);loadTileset(montain,cachedTileset,tiledMap);
+            load_terrainTransitionList(grass,water,montain,terrainTransitionList);
             std::vector<std::vector<Tiled::Tile *> > arrayTerrainTile;
             addTerrainTile(arrayTerrainTile,grass,montain);
             //Tiled::Tileset *tilesetDebug=readTileset("mapgen.tsx",&tiledMap);
@@ -711,9 +754,8 @@ int main(int argc, char *argv[])
                 {
                     Tiled::Map tiledMap(Tiled::Map::Orientation::Orthogonal,mapWidth,mapHeight,16,16);
                     QHash<QString,Tiled::Tileset *> cachedTileset;
-                    loadTileset(water,cachedTileset,tiledMap);
-                    loadTileset(grass,cachedTileset,tiledMap);
-                    loadTileset(montain,cachedTileset,tiledMap);
+                    loadTileset(water,cachedTileset,tiledMap);loadTileset(grass,cachedTileset,tiledMap);loadTileset(montain,cachedTileset,tiledMap);
+                    load_terrainTransitionList(grass,water,montain,terrainTransitionList);
                     //Tiled::Tileset *tilesetDebug=readTileset("mapgen.tsx",&tiledMap);
                     if(displayzone)
                     {
