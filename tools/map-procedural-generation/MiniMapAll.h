55aa4cbd16aacf81ba833bdaab23643c0a45ec4f
BRULE Herman <alpha_one_x86@first-world.info>
2017-08-08 13:20:43 +0200

    [Add] Mini map road (tools)

--- tools/map-procedural-generation/MiniMapAll.h
+++ tools/map-procedural-generation/MiniMapAll.h
@@ -2,12 +2,24 @@
 #define MINIMAPALL_H
 
 #include "../map-procedural-generation-terrain/MiniMap.h"
+#include <QPainter>
 
 class MiniMapAll
 {
 public:
     static QImage makeMapTiled(const unsigned int worldWidthMap, const unsigned int worldHeightMap,
                                const unsigned int mapWidth,const unsigned int mapHeight);
+    static void drawRoad(const uint8_t orientation,QPainter &p,const unsigned int x,const unsigned int y,const unsigned int mapWidth,const unsigned int mapHeight);
+
+    static QImage minimapcitybig;
+    static QImage minimapcitymedium;
+    static QImage minimapcitysmall;
+
+    static QImage minimap1way;
+    static QImage minimap2way1;
+    static QImage minimap2way2;
+    static QImage minimap3way;
+    static QImage minimap4way;
 };
 
 #endif // MINIMAPALL_H
