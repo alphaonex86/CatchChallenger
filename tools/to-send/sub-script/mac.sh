e287a0c4e54facf7ec71420e1b7b3a4f1e4e73ee
BRULE Herman <alpha_one_x86@first-world.info>
2018-03-13 02:49:59 +0100

    [Fix] For Mac (shared lib,tools)

--- tools/to-send/sub-script/mac.sh
+++ tools/to-send/sub-script/mac.sh
@@ -10,8 +10,19 @@ cd ${TEMP_PATH}/
 
 IPMAC="192.168.158.34"
 SSHUSER="user"
-QTVERSION="5.4.1"
-QTVERSIONMAJ="5.4"
+QTVERSION="5.9.4"
+QTVERSIONMAJ="5.9"
+
+echo try first connect:
+ssh user@${IPMAC} exit
+if [ `ssh user@${IPMAC} 'echo lVt75gJ4sJXjq2gWxzXd8pV8' | grep lVt75gJ4sJXjq2gWxzXd8pV8 | wc -l` -ne 1 ]
+then
+    echo no connect...
+    exit
+fi
+
+ssh user@${IPMAC} "cd /usr/bin/;ln -s /Applications/Xcode.app/Contents/Developer//Toolchains/XcodeDefault.xctoolchain/usr/bin/otool > /dev/null 2>&1"
+ssh user@${IPMAC} "mkdir /opt/ > /dev/null 2>&1;mkdir /opt/local/ > /dev/null 2>&1;mkdir /opt/local/lib/ > /dev/null 2>&1;cd /opt/local/lib/;ln -s /usr/lib/libz.1.dylib > /dev/null 2>&1"
 
 function compil {
     cd ${TEMP_PATH}/
@@ -28,16 +39,19 @@ function compil {
         find ${TEMP_PATH}/${TARGET}-mac-os-x/ -name "GeneralVariable.h" -exec sed -i "s/#define CATCHCHALLENGER_EXTRA_CHECK/\/\/#define CATCHCHALLENGER_EXTRA_CHECK/g" {} \; > /dev/null 2>&1
         echo "try connect"
         ssh ${SSHUSER}@${IPMAC} "rm -fR /Users/${SSHUSER}/Desktop/CatchChallenger/"
-        echo "try rsync"
+        echo "try rsync 1"
         ssh ${SSHUSER}@${IPMAC} "mkdir /Users/${SSHUSER}/Desktop/CatchChallenger/"
-        ssh root@${IPMAC} "chown -f -R ${SSHUSER} /Users/${SSHUSER}/Desktop/CatchChallenger/"
+        echo "try rsync 2"
+        ssh user@${IPMAC} "chown -f -R ${SSHUSER} /Users/${SSHUSER}/Desktop/CatchChallenger/"
+        echo "try rsync 3"
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/server/ ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/server/ --exclude=*.pro.user* --exclude=*Qt5-Debug --exclude=*build-* --exclude=*build-Desktop* --exclude=*Desktop-Debug*
+        echo "try rsync 4"
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/general/ ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/general/ --exclude=*.pro.user* --exclude=*Qt5-Debug --exclude=*build-* --exclude=*build-Desktop* --exclude=*Desktop-Debug*
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/client/ ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/client/ --exclude=*.pro.user* --exclude=*Qt5-Debug --exclude=*build-* --exclude=*build-Desktop* --exclude=*Desktop-Debug*
 
         echo "try qmake"
         BASEAPPNAME="catchchallenger-${TARGET}.app"
-        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Users/user/Qt${QTVERSION}/${QTVERSIONMAJ}/clang_64/bin/qmake *.pro -spec macx-clang -config release"
+        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Users/user/Qt${QTVERSION}/${QTVERSION}/clang_64/bin/qmake *.pro -spec macx-clang -config release"
         echo "try make"
         ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Applications/Xcode.app/Contents/Developer/usr/bin/gnumake -j 3 > /dev/null 2>&1"
         RETURN_CODE=$?
@@ -88,10 +102,10 @@ function compil {
             ssh ${SSHUSER}@${IPMAC} "mkdir /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/catchchallenger-${TARGET}.app/Contents/MacOS/datapack/internal/"
             rsync -art ${DATAPACK_SOURCE} ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/catchchallenger-${TARGET}.app/Contents/MacOS/datapack/internal/
         fi
-        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Users/user/Qt${QTVERSION}/${QTVERSIONMAJ}/clang_64/bin/macdeployqt ${BASEAPPNAME}/;zip -r9 catchchallenger-${TARGET}.zip catchchallenger-${TARGET}.app/ > /dev/null 2>&1"
+        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Users/user/Qt${QTVERSION}/${QTVERSION}/clang_64/bin/macdeployqt ${BASEAPPNAME}/;zip -r9 catchchallenger-${TARGET}.zip catchchallenger-${TARGET}.app/ > /dev/null 2>&1"
         rsync -art ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/catchchallenger-${TARGET}.zip ${TEMP_PATH}/${FINAL_ARCHIVE}
         if [ ! -e ${FINAL_ARCHIVE} ]; then
-            echo ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Users/user/Qt${QTVERSION}/${QTVERSIONMAJ}/clang_64/bin/macdeployqt ${BASEAPPNAME}/;zip -r9 catchchallenger-${TARGET}.zip catchchallenger-${TARGET}.app/"
+            echo ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/client/${TARGET}/;/Users/user/Qt${QTVERSION}/${QTVERSION}/clang_64/bin/macdeployqt ${BASEAPPNAME}/;zip -r9 catchchallenger-${TARGET}.zip catchchallenger-${TARGET}.app/"
             echo "${FINAL_ARCHIVE} not exists!";
             exit;
         fi
@@ -127,14 +141,14 @@ function compilserver {
         ssh ${SSHUSER}@${IPMAC} "rm -fR /Users/${SSHUSER}/Desktop/CatchChallenger/"
         echo "try rsync"
         ssh ${SSHUSER}@${IPMAC} "mkdir /Users/${SSHUSER}/Desktop/CatchChallenger/"
-        ssh root@${IPMAC} "chown -f -R ${SSHUSER} /Users/${SSHUSER}/Desktop/CatchChallenger/"
+        ssh user@${IPMAC} "chown -f -R ${SSHUSER} /Users/${SSHUSER}/Desktop/CatchChallenger/"
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/server/ ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/server/ --exclude=*.pro.user* --exclude=*Qt5-Debug --exclude=*build-* --exclude=*build-Desktop* --exclude=*Desktop-Debug*
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/general/ ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/general/ --exclude=*.pro.user* --exclude=*Qt5-Debug --exclude=*build-* --exclude=*build-Desktop* --exclude=*Desktop-Debug*
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/client/ ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/client/ --exclude=*.pro.user* --exclude=*Qt5-Debug --exclude=*build-* --exclude=*build-Desktop* --exclude=*Desktop-Debug*
 
         echo "try qmake"
         BASEAPPNAME="catchchallenger-${TARGET}.app"
-        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/server/;/Users/user/Qt${QTVERSION}/${QTVERSIONMAJ}/clang_64/bin/qmake catchchallenger-${TARGET}.pro -spec macx-clang -config release"
+        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/server/;/Users/user/Qt${QTVERSION}/${QTVERSION}/clang_64/bin/qmake catchchallenger-${TARGET}.pro -spec macx-clang -config release"
         echo "try make"
         ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/server/;/Applications/Xcode.app/Contents/Developer/usr/bin/gnumake -j 3 > /dev/null 2>&1"
         RETURN_CODE=$?
@@ -166,7 +180,7 @@ function compilserver {
         
         rsync -art ${DATAPACK_SOURCE} ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/server/catchchallenger-${TARGET}.app/Contents/MacOS/datapack/
         rsync -art ${TEMP_PATH}/${TARGET}-mac-os-x/server/databases/catchchallenger.db.sqlite ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/server/catchchallenger-${TARGET}.app/Contents/MacOS/
-        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/server/;/Users/user/Qt${QTVERSION}/${QTVERSIONMAJ}/clang_64/bin/macdeployqt ${BASEAPPNAME}/;zip -r9 catchchallenger-${TARGET}.zip catchchallenger-${TARGET}.app/ > /dev/null 2>&1"
+        ssh ${SSHUSER}@${IPMAC} "cd /Users/${SSHUSER}/Desktop/CatchChallenger/server/;/Users/user/Qt${QTVERSION}/${QTVERSION}/clang_64/bin/macdeployqt ${BASEAPPNAME}/;zip -r9 catchchallenger-${TARGET}.zip catchchallenger-${TARGET}.app/ > /dev/null 2>&1"
         rsync -art ${SSHUSER}@${IPMAC}:/Users/${SSHUSER}/Desktop/CatchChallenger/server/catchchallenger-${TARGET}.zip ${TEMP_PATH}/${FINAL_ARCHIVE}
         if [ ! -e ${FINAL_ARCHIVE} ]; then
             echo "${FINAL_ARCHIVE} not exists!";
