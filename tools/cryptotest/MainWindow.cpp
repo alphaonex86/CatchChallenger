d76a850098b1f665992dfe739a8b0bea8fd6164c
BRULE Herman <alpha_one_x86@first-world.info>
2018-05-02 15:41:34 +0200

    [Add] cryptotest

---
+++ tools/cryptotest/MainWindow.cpp
@@ -0,0 +1,104 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+#include <QMessageBox>
+#include <iostream>
+#include <stdio.h>
+
+/*Constant: ED25519_KEY_SIZE
+The size of a private or public Ed25519 key, 32 octets.
+
+Constant: ED25519_SIGNATURE_SIZE
+The size of an Ed25519 signature, 64 octets.
+
+Function: void ed25519_sha512_public_key (uint8_t *pub, const uint8_t *priv)
+Computes the public key corresponding to the given private key. Both input and output are of size ED25519_KEY_SIZE.
+
+Function: void ed25519_sha512_sign (const uint8_t *pub, const uint8_t *priv, size_t length, const uint8_t *msg, uint8_t *signature)
+Signs a message using the provided key pair.
+
+Function: int ed25519_sha512_verify (const uint8_t *pub, size_t length, const uint8_t *msg, const uint8_t *signature)
+Verifies a message using the provided public key. Returns 1 if the signature is valid, otherwise 0.*/
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    on_makekeys_clicked();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_makekeys_clicked()
+{
+    uint8_t privatekey[ED25519_KEY_SIZE];
+    uint8_t publickey[ED25519_KEY_SIZE];
+
+    FILE *ptr = fopen("/dev/random","rb");  // r for read, b for binary
+    if(ptr == NULL)
+        abort();
+    const int readSize=fread(privatekey,1,sizeof(privatekey),ptr);
+    if(readSize != sizeof(privatekey))
+        abort();
+    fclose(ptr);
+
+    ed25519_sha512_public_key(publickey,privatekey);
+
+    ui->publickey->setText(QString(QByteArray((char *)publickey,sizeof(publickey)).toBase64()));
+    ui->privatekey->setText(QString(QByteArray((char *)privatekey,sizeof(privatekey)).toBase64()));
+}
+
+void MainWindow::on_sign_clicked()
+{
+    QByteArray data=QByteArray::fromBase64(ui->sign_privatekey->text().toUtf8());
+    if(data.size()!=ED25519_KEY_SIZE)
+    {
+        QMessageBox::critical(this,"Key size","The input key size is wrong o not into base64");
+        return;
+    }
+
+    const uint8_t *privatekey=reinterpret_cast<const uint8_t *>(data.constData());
+    uint8_t publickey[ED25519_KEY_SIZE];
+    ed25519_sha512_public_key(publickey,privatekey);
+    uint8_t signature[ED25519_SIGNATURE_SIZE];
+
+    const QByteArray msg=ui->sign_input->toPlainText().toUtf8();
+
+    ed25519_sha512_sign(publickey,privatekey,msg.size(),reinterpret_cast<const uint8_t *>(msg.constData()),signature);
+
+    ui->sign_output->setText(QString(QByteArray(reinterpret_cast<char *>(signature),sizeof(signature)).toBase64()));
+}
+
+void MainWindow::on_verify_clicked()
+{
+    const QByteArray msg=ui->verify_input->toPlainText().toUtf8();
+
+    QByteArray data=QByteArray::fromBase64(ui->verify_publickey->text().toUtf8());
+    if(data.size()!=ED25519_KEY_SIZE)
+    {
+        QMessageBox::critical(this,"Key size","The input key size is wrong o not into base64");
+        return;
+    }
+    const uint8_t *publickey=reinterpret_cast<const uint8_t *>(data.constData());
+
+    QByteArray datasignature=QByteArray::fromBase64(ui->verify_firm->text().toUtf8());
+    if(datasignature.size()!=ED25519_SIGNATURE_SIZE)
+    {
+        QMessageBox::critical(this,"Key size","The input key size is wrong o not into base64");
+        return;
+    }
+    const uint8_t *signature=reinterpret_cast<const uint8_t *>(datasignature.constData());
+
+    const int rc = ed25519_sha512_verify (publickey,msg.size(),reinterpret_cast<const uint8_t *>(msg.constData()),signature);
+
+    if(rc == 1) {
+        QMessageBox::information(this,"Verification","Verified signature");
+    } else {
+        QMessageBox::critical(this,"Verification","Failed to verify signature, return code "+QString::number(rc));
+    }
+}
