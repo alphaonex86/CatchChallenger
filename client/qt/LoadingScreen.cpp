8ab587afb83698a0ba10b973c73b83ff4fe8ef19
BRULE Herman <alpha_one_x86@first-world.info>
2019-07-29 22:50:58 -0400

    [Change] Loading screen finish

--- client/qt/LoadingScreen.cpp
+++ client/qt/LoadingScreen.cpp
@@ -6,6 +6,30 @@ LoadingScreen::LoadingScreen(QWidget *parent) :
     ui(new Ui::LoadingScreen)
 {
     ui->setupUi(this);
+
+    widget = new CCWidget(this);
+    widget->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::MinimumExpanding);
+    widget->setMinimumSize(QSize(120, 120));
+    widget->setMaximumWidth(600);
+    horizontalLayout = new QHBoxLayout(widget);
+    horizontalLayout->setSpacing(20);
+    horizontalLayout->setContentsMargins(24, 24, 24, 24);
+    teacher = new QLabel(widget);
+    teacher->setMaximumSize(QSize(189, 206));
+    teacher->setMinimumSize(QSize(189, 206));
+    teacher->setPixmap(QPixmap(":/images/interface/teacher.png"));
+    horizontalLayout->addWidget(teacher);
+    info = new QLabel(widget);
+    horizontalLayout->addWidget(info);
+    info->setText(tr("%1 is loading...").arg("<b>CatchChallenger</b>"));
+    info->setStyleSheet("color:#401c02;");
+
+    ui->horizontalLayout_2->addWidget(widget);
+    progressbar=new CCprogressbar(this);
+    progressbar->setMaximum(100);
+    progressbar->setMinimum(0);
+    progressbar->setValue(0);
+    ui->verticalLayout->insertWidget(ui->verticalLayout->count(),progressbar);
 }
 
 LoadingScreen::~LoadingScreen()
@@ -15,8 +39,27 @@ LoadingScreen::~LoadingScreen()
 
 void LoadingScreen::resizeEvent(QResizeEvent *)
 {
-    if(width()<600)
-        ui->teacher->setVisible(false);
+    if(width()<600 || height()<600)
+    {
+        teacher->setVisible(false);
+        widget->setMinimumHeight(120);
+    }
     else
-        ui->teacher->setVisible(true);
+    {
+        teacher->setVisible(true);
+        widget->setMinimumHeight(206+24*2);
+    }
+
+    if(height()<500)
+    {
+        progressbar->setMinimumHeight(0);
+    }
+    else if(height()<800)
+    {
+        progressbar->setMinimumHeight(45);
+    }
+    else
+    {
+        progressbar->setMinimumHeight(55);
+    }
 }
