86b883c739d8eee2f0c8f8ec8d09f02fb0420630
BRULE Herman <alpha_one_x86@first-world.info>
2019-10-14 09:08:20 -0400

    [Fix] For android (client)

--- client/qt/Settings.cpp
+++ client/qt/Settings.cpp
@@ -1,13 +1,15 @@
 #include "Settings.h"
 #include <QStandardPaths>
+#include <QDir>
 
-#ifdef Q_OS_ANDROID
-if (QSettings().fileName().startsWith("/.config"))
-
-{ QSettings::setPath(QSettings::NativeFormat, QSettings::UserScope, QDir::homePath() + "/.config"); }
-#endif
 QSettings Settings::settings(QStandardPaths::standardLocations(QStandardPaths::DataLocation).first(),QSettings::IniFormat);
 
 Settings::Settings()
 {
+    #ifdef Q_OS_ANDROID
+    /*if(QSettings().fileName().startsWith("/.config"))
+    {
+        QSettings::setPath(QSettings::NativeFormat, QSettings::UserScope, QDir::homePath() + "/.config");
+    }*/
+    #endif
 }
