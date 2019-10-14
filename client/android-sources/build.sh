2425923c86321ac5410bf0a6098d4b6faec8f4be
BRULE Herman <alpha_one_x86@first-world.info>
2019-10-14 15:32:37 -0400

    [Fix] Fix for build

--- client/android-sources/build.sh
+++ client/android-sources/build.sh
@@ -5,6 +5,6 @@ cd /home/user/
 mkdir /home/user/build && cd /home/user/build
 qmake -r /home/user/src/client/catchchallenger.pro ANDROID_EXTRA_LIBS+=$ANDROID_DEV/lib/libcrypto.so ANDROID_EXTRA_LIBS+=$ANDROID_DEV/lib/libssl.so
 make -j5
-make install INSTALL_ROOT=/home/user/build/dist/
-make install INSTALL_ROOT=/home/user/src/dist/
+#make install INSTALL_ROOT=/home/user/build/dist/
+make install INSTALL_ROOT=/home/user/dist/
 androiddeployqt --input android-libcatchchallenger.so-deployment-settings.json --output dist/ --android-platform 28 --deployment bundled --gradle --release
