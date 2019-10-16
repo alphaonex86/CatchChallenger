c951a2712e78f6f7aa4beb2bdd1697cfc4d456ba
BRULE Herman <alpha_one_x86@first-world.info>
2019-10-15 21:04:07 -0400

    [Fix] Disable background animation (crash)

--- client/qt/CCBackground.cpp
+++ client/qt/CCBackground.cpp
@@ -16,7 +16,7 @@ CCBackground::CCBackground(QWidget *parent) :
     connect(&treebackTimer,&QTimer::timeout,this,&CCBackground::treebackSlot);
     connect(&treefrontTimer,&QTimer::timeout,this,&CCBackground::treefrontSlot);
     connect(&updateTimer,&QTimer::timeout,this,&CCBackground::update);
-    startAnimation();
+    //startAnimation();//crash on android
 }
 
 void CCBackground::startAnimation()
