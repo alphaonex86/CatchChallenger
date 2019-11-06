4821f33466fab627c6e239917e4cb0439da7ce53
BRULE Herman <alpha_one_x86@first-world.info>
2019-11-05 20:47:43 -0400

    [Fix] More clean for OpenGL (client)

--- client/catchchallenger.pro
+++ client/catchchallenger.pro
@@ -1,13 +1,5 @@
-include(qt/client.pri)
+include(qtopengl/client.pri)
 include(../general/general.pri)
-include(qt/multi.pri)
-
-!contains(DEFINES, NOSINGLEPLAYER) {
-    include(qt/solo.pri)
-    include(../server/catchchallenger-server-qt.pri)
-    INCLUDEPATH += -I/usr/include/
-}
 
 TARGET = catchchallenger
 
-SOURCES += qtopengl/main.cpp
--- client/qtopengl/CCBackground.cpp
+++ client/qtopengl/CCBackground.cpp
@@ -1,24 +1,36 @@
 #include "CCBackground.h"
 #include <QPainter>
 #include <QTime>
+#include <chrono>
+#include <iostream>
 
-CCBackground::CCBackground(QWidget *parent) :
-    QWidget(parent)
+unsigned int CCBackground::timeToDraw;
+unsigned int CCBackground::timeDraw;
+std::chrono::time_point<std::chrono::steady_clock> CCBackground::timeFromStart;
+
+CCBackground::CCBackground(QGraphicsItem *parent) :
+    QGraphicsItem(parent)
 {
     zoom=0;
     benchmark=true;
-    setMinimumHeight(120);
-    setMinimumWidth(120);
     grassMove=0;
     treebackMove=0;
     treefrontMove=0;
-    connect(&grassTimer,&QTimer::timeout,this,&CCBackground::grassSlot);
-    connect(&treebackTimer,&QTimer::timeout,this,&CCBackground::treebackSlot);
-    connect(&treefrontTimer,&QTimer::timeout,this,&CCBackground::treefrontSlot);
-    connect(&updateTimer,&QTimer::timeout,this,&CCBackground::update);
-    #ifndef Q_OS_ANDROID
+    if(!QObject::connect(&grassTimer,&QTimer::timeout,this,&CCBackground::grassSlot))
+        abort();
+    if(!QObject::connect(&treebackTimer,&QTimer::timeout,this,&CCBackground::treebackSlot))
+        abort();
+    if(!QObject::connect(&treefrontTimer,&QTimer::timeout,this,&CCBackground::treefrontSlot))
+        abort();
+    if(!QObject::connect(&updateTimer,&QTimer::timeout,this,&CCBackground::updateSlot))
+        abort();
     startAnimation();
-    #endif
+    timeFromStart=std::chrono::steady_clock::now();
+}
+
+QRectF CCBackground::boundingRect() const
+{
+    return QRectF();
 }
 
 void CCBackground::startAnimation()
@@ -43,9 +55,9 @@ void CCBackground::scalePix(QPixmap &pix,unsigned int zoom)
     pix=pix.scaled(pix.width()*zoom,pix.height()*zoom);
 }
 
-void CCBackground::update()
+void CCBackground::updateSlot()
 {
-    QWidget::update();
+    update();
 }
 
 void CCBackground::grassSlot()
@@ -53,9 +65,6 @@ void CCBackground::grassSlot()
     if(grass.isNull())
         return;
     grassMove++;
-    unsigned int targetZoom=getTargetZoom();
-    if((unsigned int)grassMove>=grass.width()*targetZoom)
-        grassMove=0;
 }
 
 void CCBackground::treebackSlot()
@@ -63,9 +72,6 @@ void CCBackground::treebackSlot()
     if(treeback.isNull())
         return;
     treebackMove++;
-    unsigned int targetZoom=getTargetZoom();
-    if((unsigned int)treebackMove>=treeback.width()*targetZoom)
-        treebackMove=0;
 }
 
 void CCBackground::treefrontSlot()
@@ -73,19 +79,16 @@ void CCBackground::treefrontSlot()
     if(treefront.isNull())
         return;
     treefrontMove++;
-    unsigned int targetZoom=getTargetZoom();
-    if((unsigned int)treefrontMove>=treefront.width()*targetZoom)
-        treefrontMove=0;
 }
 
-unsigned int CCBackground::getTargetZoom()
+unsigned int CCBackground::getTargetZoom(QWidget *widget)
 {
-    unsigned int targetZoomHeight=(unsigned int)height()/120;
+    unsigned int targetZoomHeight=(unsigned int)widget->height()/120;
     if(targetZoomHeight<1)
         targetZoomHeight=1;
     if(targetZoomHeight>8)
         targetZoomHeight=8;
-    unsigned int targetZoomWidth=(unsigned int)width()/120;
+    unsigned int targetZoomWidth=(unsigned int)widget->width()/120;
     if(targetZoomWidth<1)
         targetZoomWidth=1;
     if(targetZoomWidth>8)
@@ -96,11 +99,11 @@ unsigned int CCBackground::getTargetZoom()
     return targetZoom;
 }
 
-void CCBackground::paintEvent(QPaintEvent *)
+void CCBackground::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QTime time;
     time.restart();
-    unsigned int targetZoom=getTargetZoom();
+    unsigned int targetZoom=getTargetZoom(widget);
     if(zoom!=targetZoom)
     {
         //std::cout << "new zoom: " << targetZoom << " with " << width() << "*" << height() << std::endl;
@@ -132,81 +135,57 @@ void CCBackground::paintEvent(QPaintEvent *)
         treefrontMove=rand()%treefront.width()*targetZoom;
         zoom=targetZoom;
     }
-    QPainter paint;
-    paint.begin(this);
+    treefrontMove%=treefront.width()*targetZoom;
+    treebackMove%=treeback.width()*targetZoom;
+    grassMove%=grass.width()*targetZoom;
     //paint.fillRect(rect(),QColor(116,225,255));
 
     int grassOffset=0;
     int skyOffset=0;
     int sunOffset=0;
     unsigned int endOfGrass=0;
-    if((unsigned int)height()<=120*targetZoom)
-        endOfGrass=height();
+    if((unsigned int)widget->height()<=120*targetZoom)
+        endOfGrass=widget->height();
     else
     {
-        skyOffset=(height()-120*targetZoom)*2/3/*66%*/;
-        if(skyOffset>height()/4)
-            skyOffset=height()/4;
-        endOfGrass=120*targetZoom+(height()-120*targetZoom)*2/3/*66%*/;
-        paint.fillRect(0,endOfGrass,width(),height()-endOfGrass,QColor(131,203,83));
-        paint.fillRect(0,0,width(),skyOffset,QColor(115,225,255));
+        skyOffset=(widget->height()-120*targetZoom)*2/3/*66%*/;
+        if(skyOffset>widget->height()/4)
+            skyOffset=widget->height()/4;
+        endOfGrass=120*targetZoom+(widget->height()-120*targetZoom)*2/3/*66%*/;
+        painter->fillRect(0,endOfGrass,widget->width(),widget->height()-endOfGrass,QColor(131,203,83));
+        painter->fillRect(0,0,widget->width(),skyOffset,QColor(115,225,255));
     }
     sunOffset=endOfGrass/6/*16%*/;
 
-    QRect gradientRect(0,skyOffset,width(),120*targetZoom);
+    QRect gradientRect(0,skyOffset,widget->width(),120*targetZoom);
     QLinearGradient gradient(gradientRect.topLeft(), gradientRect.bottomLeft()); // diagonal gradient from top-left to bottom-right
     gradient.setColorAt(0, QColor(115,225,255));
     gradient.setColorAt(1, QColor(183,241,255));
-    paint.fillRect(0,skyOffset,width(),endOfGrass-skyOffset,gradient);
+    painter->fillRect(0,skyOffset,widget->width(),endOfGrass-skyOffset,gradient);
 
     grassOffset=endOfGrass-grass.height();
     unsigned int treeFrontOffset=grassOffset+4*targetZoom-treefront.height();
     unsigned int treebackOffset=grassOffset+4*targetZoom-treeback.height();
 
     int lastTreebackX=-treebackMove;
-    while(width()>lastTreebackX)
+    while(widget->width()>lastTreebackX)
     {
-        paint.drawPixmap(lastTreebackX,treebackOffset,treeback.width(),    treeback.height(),    treeback);
+        painter->drawPixmap(lastTreebackX,treebackOffset,treeback.width(),    treeback.height(),    treeback);
         lastTreebackX+=treeback.width();
     }
     int lastTreefrontX=-treefrontMove;
-    while(width()>lastTreefrontX)
+    while(widget->width()>lastTreefrontX)
     {
-        paint.drawPixmap(lastTreefrontX,treeFrontOffset,treefront.width(),    treefront.height(),    treefront);
+        painter->drawPixmap(lastTreefrontX,treeFrontOffset,treefront.width(),    treefront.height(),    treefront);
         lastTreefrontX+=treefront.width();
     }
     int lastGrassX=-grassMove;
-    while(width()>lastGrassX)
+    while(widget->width()>lastGrassX)
     {
-        paint.drawPixmap(lastGrassX,grassOffset,grass.width(),    grass.height(),    grass);
+        painter->drawPixmap(lastGrassX,grassOffset,grass.width(),    grass.height(),    grass);
         lastGrassX+=grass.width();
     }
 
-    paint.drawPixmap(width()*2/3/*66%*/-sun.width()/2,sunOffset,sun.width(),    sun.height(),    sun);
-    paint.drawPixmap(width()/3/*33%*/-cloud.width()/2,skyOffset+(endOfGrass-skyOffset)/4/*16%*/,cloud.width(),    cloud.height(),    cloud);
-
-    if(benchmark)
-    {
-        results.push_back(time.elapsed());
-        if(results.size()*updateTimer.interval()>1000)/*benchmark on 1s*/
-        {
-            benchmark=false;
-            unsigned int index=0;
-            unsigned int minimalTime=(unsigned int)updateTimer.interval()/2;
-            while(index<results.size())
-            {
-                if(results.at(index)<minimalTime)
-                    break;
-                index++;
-            }
-            if(index>=results.size())
-            {
-                grassTimer.stop();
-                treebackTimer.stop();
-                treefrontTimer.stop();
-                updateTimer.stop();
-            }
-            results.clear();
-        }
-    }
+    painter->drawPixmap(widget->width()*2/3/*66%*/-sun.width()/2,sunOffset,sun.width(),    sun.height(),    sun);
+    painter->drawPixmap(widget->width()/3/*33%*/-cloud.width()/2,skyOffset+(endOfGrass-skyOffset)/4/*16%*/,cloud.width(),    cloud.height(),    cloud);
 }
