f4d5542d750df79f115c7a4f01431a293f369f75
BRULE Herman <alpha_one_x86@first-world.info>
2022-01-17 20:35:52 -0400

    [Clean] Clean

--- tools/testOpenGL/datapack/building-small.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="building-small" tilewidth="16" tileheight="16">
- <image source="building-small.png" trans="000000" width="512" height="512"/>
-</tileset>
